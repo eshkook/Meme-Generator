893529e0c5e31c5b2d4260d5c930cf8a2b7274c5
eshkook <101457806+eshkook@users.noreply.github.com>
2023-08-31 11:41:41 +0300

    sdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import Logo from "../images/pig_2.jpg"
+import Logo from "../images/pig.png"
 
 export default function Header() {
   return (
     <header>
       <img style={{ backgroundColor: "transparent" }} src={Logo} />
       <h2>Meme Generator</h2>
-      <h4>React Course - Project 3</h4>
+      {/* <h4>React Course - Project 3</h4> */}
     </header>
     
   )
