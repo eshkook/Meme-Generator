df112232b1ed7199784b14a8c10bdecd151616ef
itai <102740785+NoamMesh@users.noreply.github.com>
2023-09-12 12:31:12 +0300

    wrt

---
+++ study/2. promise.js
@@ -0,0 +1,6 @@
+stock = {
+    fruit: ['lemon', 'strawberry'],
+    topping: ['chocolate', 'vanila']
+}
+
+let is_shop_open = true
--- try_api.js
+++ try_api.js
@@ -2,7 +2,10 @@
 //   .then(res => res.json())
 //   .then(data => console.log(data))
 
-fetch("http://api.imgflip.com/get_memes")
-.then(res => res.json())
-.then(data => console.log(data.data.memes[0].url))  
+// fetch("http://api.imgflip.com/get_memes")
+// .then(res => res.json())
+// .then(data => console.log(data.data.memes[0].url))  
 
+fetch("http://127.0.0.1:8000/api/rooms/4/")
+.then(res => res.json())
+.then(data => console.log(data)) 
