16c13ba9977ef1e9fb56948ef3589e4a21a34739
itai <102740785+NoamMesh@users.noreply.github.com>
2023-09-12 14:27:31 +0300

    df

--- study/2_promise.js
+++ study/2_promise.js
@@ -18,10 +18,16 @@ let order = (time, work) =>    {
                       )
                                }
 
-order(2000, () => `${stock.fruits[0]} was selected`)       
+fruit_index = 0
+// assuming preparing ice cream is the following process one by one(no asynchronous actions):
+// 1. select the fruit takes 2 seconds
+// 2. reporting that the process itself begins takes 0 seconds
+// 3. cutting the fruit takes 3 seconds
+order(2000, () => `${stock.fruits[fruit_index]} was selected`)       
 .then(console.log)
 .then( ()=> {
-    return order(1000, () => console.log('production has started'))  
+    return order(0, () => console.log('production has started'))  
 })
+.then( ()=>  order(3000, () => console.log(`${stock.fruits[fruit_index]} was cut`)))
 .catch(console.log)
 
