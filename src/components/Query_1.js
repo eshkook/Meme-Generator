602dd0f1feb1693d1093b29f9d19f4d5185dcadb
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-10 09:55:12 +0300

    ed

--- src/components/Query_1.js
+++ src/components/Query_1.js
@@ -1,4 +1,4 @@
-import { useQuery, useEffect } from "@tanstack/react-query"
+import { useQuery, useMutation } from "@tanstack/react-query"
 
 const POSTS = [
   {id: 1, title: "post 1"},
@@ -6,9 +6,16 @@ const POSTS = [
 ]
 
 export default function Query_1() {
-  useQuery({
+  const postsQuery = useQuery({
     queryKey: ["posts"], // a unique identifier for youe query
+    // queryFn: () => Promise.reject("Error Message"),
+    queryFn: () => wait(1000).then(() => [...POSTS]),
   })
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>
+  }
 
   return (
     <h1>
