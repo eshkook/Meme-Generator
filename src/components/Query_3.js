df62865ce509569fc78e503128ac78f3e85a5644
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-10 14:28:50 +0300

    f

--- src/components/Query_3.js
+++ src/components/Query_3.js
@@ -1,17 +1,17 @@
 import { useState } from "react"
-import PostLists1 from "./PostLists1.js"
-import PostLists2 from "./PostLists2.js"
+import PostsList1 from "./PostsList1.js"
+import PostsList2 from "./PostsList2.js"
 
 export default function Query_3() {
-    const [currentPage, setCurrentPage] = useState(<PostLists1 />)
+    const [currentPage, setCurrentPage] = useState(<PostsList1 />)
 
   
     return (
         <div>
-            <button onClick={() => setCurrentPage(<PostLists1 />)}>
+            <button onClick={() => setCurrentPage(<PostsLists />)}>
                 Posts List 1
             </button>
-            <button onClick={() => setCurrentPage(<PostLists2 />)}>
+            <button onClick={() => setCurrentPage(<PostsList2 />)}>
                 Posts List 2
             </button>
             <br />
