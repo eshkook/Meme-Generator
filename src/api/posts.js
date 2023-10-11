4e76a4d2953a396c3801a3d0cd909a2c27bf0307
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-11 17:17:49 +0300

    t

--- src/api/posts.js
+++ src/api/posts.js
@@ -6,6 +6,21 @@ export function getPosts() {
     .then(res => res.data)
 }
 
+export function getPost(id) {
+  return axios.get(`https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/posts/${id}`).then(res => res.data)
+}
+
+export function createPost({ title, body }) {
+  return axios
+    .post("https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/posts/", {
+      title,
+      body,
+      userId: 1,
+      id: Date.now(),
+    })
+    .then(res => res.data)
+}
+
 // export function getPostsPaginated(page) {
 //   return axios
 //     .get("http://localhost:3000/posts", {
@@ -21,17 +36,5 @@ export function getPosts() {
 //     })
 // }
 
-// export function getPost(id) {
-//   return axios.get(`http://localhost:3000/posts/${id}`).then(res => res.data)
-// }
 
-// export function createPost({ title, body }) {
-//   return axios
-//     .post("http://localhost:3000/posts", {
-//       title,
-//       body,
-//       userId: 1,
-//       id: Date.now(),
-//     })
-//     .then(res => res.data)
-// }
+
--- src/components/Query_3.js
+++ src/components/Query_3.js
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import PostsList1 from "./PostsList1.js"
 import PostsList2 from "./PostsList2.js"
+import Post from "./Post.js"
+import CreatePost from "./CreatePost.js"
 
 export default function Query_3() {
     const [currentPage, setCurrentPage] = useState(<PostsList1 />)
@@ -12,6 +14,12 @@ export default function Query_3() {
             </button>
             <button onClick={() => setCurrentPage(<PostsList2 />)}>
                 Posts List 2
+            </button>
+            <button onClick={() => setCurrentPage(<Post />)}>
+                Post
+            </button>
+            <button onClick={() => setCurrentPage(<CreatePost />)}>
+                Create Post
             </button>
             <br />
             {currentPage}
