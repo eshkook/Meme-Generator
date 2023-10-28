7323db40c6cf5084308e06b15f5519f103127ad2
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-28 16:55:50 +0300

    d

---
+++ src/components_authentication/Login.js
@@ -0,0 +1,18 @@
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button';
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useState } from "react"
+
+export default function SignUp() {
+
+    
+  
+  return (
+      <>
+          
+      </>
+  )
+}
+
+
--- src/components_authentication/SignUp.js
+++ src/components_authentication/SignUp.js
@@ -1,14 +1,14 @@
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button';
-import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react"
 import { signup_post } from "../api/posts.js";
 
 export default function SignUp() {
 
     const createPostMutation = useMutation({
-        mutationFn: createPost,
+        mutationFn: signup_post,
         onSuccess: data => {
             pass //??????????????????????????????????????????????????
         },
@@ -104,77 +104,3 @@ export default function SignUp() {
   )
 }
 
-
-
-//     const [formState, setFormState] = useState({
-//         username: '',
-//         password: '',
-
-//       })
-
-//     const [fieldErrorState, setFieldError] = useState({
-//         Username: false,
-//         Password: false
-//       })  
-
-//       function updateFormState(event) { 
-//         const {name, value} = event.target
-//         setFormState(prevFormState => ({
-//           ...prevFormState, 
-//           [name]: value
-//         }))
-//       }
-    
-//       function handleSubmit(event) { 
-//         event.preventDefault() // preventing re-rendering the page 
-//         setFieldError({
-//             Username: (formState.Username===''),
-//             Password: (formState.Password==='')
-//         })
-
-//         if (fieldErrorState.Username || fieldErrorState.Password) {
-//             console.log(formState)
-//             createPostMutation.mutate({
-//               title: titleRef.current.value,
-//               body: bodyRef.current.value,
-//               userId: userIdInt,  // include userId in the mutation data
-//           });
-//         }    
-//       }
-    
-//     return (
-//         <>
-//             <Typography variant="subtitle1" component="h1">
-//                 Login:
-//             </Typography>
-//             <br />
-
-//             <form onSubmit={handleSubmit} noValidate autoComplete='off'>
-//                 {/* noValidate makes the browser not use its built-in validation messages as we want to do it ourselves, 
-//             autoComplete off makes it not complete the user's text */}
-
-//                 <TextField
-//                     onChange={updateFormState} // same as writing onChange={()=>updateFormState(event)}
-//                     id="outlined-basic"
-//                     label="Username"
-//                     variant="outlined"
-//                     name="Username"
-//                     value={formState.Username}
-//                     error = {fieldErrorState.Username}
-//                     // required  // make a '*' to indicate it is a mandatory field
-//                      />
-//                 <TextField
-//                     onChange={updateFormState}
-//                     id="outlined-basic"
-//                     label="Password"
-//                     variant="outlined"
-//                     name="Password"
-//                     value={formState.Password}
-//                     error = {fieldErrorState.Password}
-//                     // required  // make a '*' to indicate it is a mandatory field
-//                      />
-//                 <Button variant="contained" type='submit'>Submit</Button>
-//             </form>
-//         </>
-//     )
-// }
