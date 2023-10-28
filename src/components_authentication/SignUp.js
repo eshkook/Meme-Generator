8d1c1aec764f1714a73956e2eb39a0f0f99f8ad1
itai <102740785+NoamMesh@users.noreply.github.com>
2023-10-28 16:04:23 +0300

    d

--- src/components_authentication/SignUp.js
+++ src/components_authentication/SignUp.js
@@ -1,115 +1,91 @@
-import { useState, useEffect } from "react"
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button';
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { useState } from "react"
 
-export default function signUp() {
+export default function SignUp() {
 
-  const [formState, setFormState] = useState({
-    topText: '',
-    bottomText: '',
-    comments: '',
-    isFriendly: true,
-    employment: "",
-    favCol: ''
-  })
+    const [formState, setFormState] = useState({
+        username: '',
+        password: '',
+        password_confirmation: '',
+        hobbies: '',
+        age: null
+      })
 
-  function updateFormState(event) { 
-    const {name, value, type, checked} = event.target
-    setFormState(prevFormState => ({
-      ...prevFormState, 
-      [name]: type === "checkbox" ? checked : value
-    }))
-  }
+    const [fieldErrorState, setFieldError] = useState({
+        username: false,
+        password: false,
+        password_confirmation: false,
+      })  
 
-  function handleSubmit(event) { 
-    event.preventDefault() // preventing re-rendering the page 
-    console.log(formState)
-    // api send the data submitted
-  }
-
-  return (
-    <div className="form-practice">
-      <form onSubmit={handleSubmit} className="form">
-        <div className="input-text-row">
-          <input onChange={updateFormState} 
-                 className="top-text-bar" 
-                 type="text" 
-                 placeholder="top text" 
-                 name="topText"
-                 value={formState.topText}/> {/* this make us have a single source of truth rather than thae state and the value being a duplicate of information. makes our code faster */}
-        </div>
-        <div className="input-text-row">
-          <input onChange={updateFormState} 
-                 className="bottom-text-bar" 
-                 type="text" 
-                 placeholder="bottom text" 
-                 name="bottomText"
-                 value={formState.bottomText}/>
-        </div>  
-        <div className="input-text-row">
-          {/* text area" */}
-          <textarea onChange={updateFormState} 
-                      placeholder="comments" 
-                      name="comments"
-                      value={formState.comments}/>   
-        </div>     
-        <div className="input-text-row">
-          {/* checkbox. it is tied to the checkbox above because we write htmlFor="isFriendly" */}
-          <input type="checkbox"
-                 onChange={updateFormState} 
-                 name="isFriendly"
-                 checked={formState.isFriendly}/> 
-          <label htmlFor="isFriendly">Are you friendly</label>         
-        </div>
-        <div className="input-text-row">
-          {/* radio button: */}
-          <fieldset>
-            <legend>Current Employment Status</legend>
-            <input type="radio"
-                 id="unemployed"
-                 name="employment"
-                 value="unemployed"
-                 checked={formState.employment === "unemployed"} // single source of truth 
-                 onChange={updateFormState}
-                 /> 
-            <label htmlFor="unemployed">Unemployed</label>
+      function updateFormState(event) { 
+        const {name, value} = event.target
+        setFormState(prevFormState => ({
+          ...prevFormState, 
+          [name]: value
+        }))
+      }
+    
+      function handleSubmit(event) { 
+        event.preventDefault() // preventing re-rendering the page
+        const temp_object = {
+          username: (formState.username == ''),
+          password: (formState.password == ''),
+          password_confirmation: (formState.password_confirmation != formState.password || formState.password_confirmation == ''), 
+      } 
+        setFieldError(temp_object)
+        
+        if (!(temp_object.username || temp_object.password || temp_object.password_confirmation)) {
+            console.log(formState)
+            // api send the data submitted
+        }    
+      }
+    
+    return (
+        <>
+            <Typography variant="subtitle1" component="h1">
+                Sign-Up:
+            </Typography>
             <br />
-            <input type="radio"
-                 id="part-time"
-                 name="employment"
-                 value="part-time"
-                 checked={formState.employment === "part-time"}
-                 onChange={updateFormState}
-                 /> 
-            <label htmlFor="part-time">Part-time</label>
-            <br />      
-            <input type="radio"
-                 id="full-time"
-                 name="employment"
-                 value="full-time"
-                 checked={formState.employment === "full-time"}
-                 onChange={updateFormState}
-                 /> 
-            <label htmlFor="full-time">Full-time</label>
-            <br />  
-          </fieldset>
-        </div>  
-        <div className="input-text-row">
-          {/* checkbox. it is tied to the checkbox above because we write htmlFor="isFriendly" */}
-          <select 
-                 onChange={updateFormState} 
-                 id="favCol"
-                 name="favCol"
-                 value={formState.favCol}>
-            <option value="">---Choose---</option>      
-            <option value="red">Red</option>
-            <option value="blue">Blue</option>  
-            <option value="green">Green</option>
-          </select>     
-          <label htmlFor="favCol">Your favorite color</label>         
-        </div>
-        <button className="get-new-image-button">  {/* a button inside a form is automatically of type 'submit */}
-          <p>Submit</p>
-        </button>                                    
-      </form>
-    </div>
-  )
+
+            <form onSubmit={handleSubmit} noValidate autoComplete='off'>
+                {/* noValidate makes the browser not use its built-in validation messages as we want to do it ourselves, 
+            autoComplete off makes it not complete the user's text */}
+
+                <TextField
+                    onChange={updateFormState} // same as writing onChange={()=>updateFormState(event)}
+                    id="outlined-basic"
+                    label="Username"
+                    variant="outlined"
+                    name="username"
+                    value={formState.username}
+                    error = {fieldErrorState.username}
+                    // required  // make a '*' to indicate it is a mandatory field
+                     />
+                <TextField
+                    onChange={updateFormState}
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    name="password"
+                    value={formState.password}
+                    error = {fieldErrorState.password}
+                    // required  // make a '*' to indicate it is a mandatory field
+                     />
+                <TextField
+                    onChange={updateFormState}
+                    id="outlined-basic"
+                    label="Password Confirmation"
+                    variant="outlined"
+                    name="password_confirmation"
+                    value={formState.password_confirmation}
+                    error = {fieldErrorState.password_confirmation}
+                    // required  // make a '*' to indicate it is a mandatory field
+                     />     
+                <Button variant="contained" type='submit'>Submit</Button>
+            </form>
+        </>
+    )
 }
