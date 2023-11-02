4878ecae131d6a470c3211054bd81aab115d354f
itai <102740785+NoamMesh@users.noreply.github.com>
2023-11-02 11:55:44 +0200

    f

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,65 +3,58 @@ import axios from "axios"
 export function signup_post({ username, password, hobbies, age }) {
   return axios
     .post("https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/signup/", {
-        username,
-        password,
-        hobbies,
-        age
+      username,
+      password,
+      hobbies,
+      age
     })
     .then(res => res.data)
     .catch(error => {
-        // Propagate the error to react-query
-        throw error.response ? error.response.data : new Error('Network error');
+      // Propagate the error to react-query
+      throw error.response ? error.response.data : new Error('Network error');
     });
 }
 
-export function login_post({ username, password }) { 
+export function login_post({ username, password }) {
   return axios
-      .post("https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/login/", {
-          username,
-          password
-      })
-      .then(res => res.data)
-      .catch(error => {
-          // Propagate the error to react-query
-          throw error.response ? error.response.data : new Error('Network error');
-      });
+    .post("https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/login/", {
+      username,
+      password
+    })
+    .then(res => res.data)
+    .catch(error => {
+      // Propagate the error to react-query
+      throw error.response ? error.response.data : new Error('Network error');
+    });
 }
 
-export function logout_post() { 
-  return axios
-      .post("https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/logout/")
-      .then(res => res.data)
-      .catch(error => {
-          // Propagate the error to react-query
-          throw error.response ? error.response.data : new Error('Network error');
-      });
-}
-
-// export function logout_post() { 
-//   return axios.post("https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/logout/")
-//       .then(res => res.data);
-// }
-
-// export function logout_post() { 
+// export function logout_post() {
 //   return axios
 //     .post("https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/logout/")
-//     .then(res => {
-//       if (!res.status || res.status < 200 || res.status >= 300) {
-//         // Throw an error if the status code is not in the 2xx range
-//         throw new Error(`Request failed with status code ${res.status}`);
-//       }
-//       return res.data;
-//     })
+//     .then(res => res.data)
 //     .catch(error => {
-//         // Propagate the error to react-query
-//         throw error.response ? error.response.data : new Error('Network error');
+//       // Propagate the error to react-query
+//       throw error.response ? error.response.data : new Error('Network error');
 //     });
 // }
 
-// export function get_response_count({ count }) {
+export function logout_post() {
+  return axios
+    .post(
+      "https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/logout/",
+      {},
+      { withCredentials: true }  // Include this option to send cookies
+    )
+    .then(res => res.data)
+    .catch(error => {
+      throw error.response ? error.response.data : new Error('Network error');
+    });
+}
+
+
+// export function get_response_count(count) {
 //   return axios
-//     .get('https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/getresponsecount/', { count })
+//     .post('https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/getresponsecount/', { count })
 //     .then(res => res.data)
 //     .catch(error => {
 //         // Propagate the error to react-query
@@ -71,18 +64,22 @@ export function logout_post() {
 
 export function get_response_count(count) {
   return axios
-    .post('https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/getresponsecount/', { count })
+    .post(
+      'https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/getresponsecount/',
+      { count },
+      { withCredentials: true }  // Include this option to send cookies
+    )
     .then(res => res.data)
     .catch(error => {
-        // Propagate the error to react-query
-        throw error.response ? error.response.data : new Error('Network error');
+      throw error.response ? error.response.data : new Error('Network error');
     });
 }
 
+
 export function getImage() {
   return axios
-  .get('https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/image/', { timeout: 5000 })
-  .then(res => res.data)
+    .get('https://v9m2jp3tgz.eu-west-1.awsapprunner.com/api/image/', { timeout: 5000 })
+    .then(res => res.data)
 }
 
 export function getPosts() {
